5d23e4c18a7bf2c74c3c7710771732f43b2c8393
Ken Eucker <keneucker@gmail.com>
2019-04-30 22:31:05 -0700

    Added the changes from the copyAllToDist feature branch for testing.

--- scripts/build.js
+++ scripts/build.js
@@ -4,40 +4,43 @@
  * The create-guten-block CLI builds here.
  */
 
-'use strict';
+'use strict'
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+process.env.BABEL_ENV = 'production'
+process.env.NODE_ENV = 'production'
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
 process.on( 'unhandledRejection', err => {
-	throw err;
-} );
+	throw err
+} )
 
 // Modules.
-const fs = require( 'fs' );
-const ora = require( 'ora' );
-const path = require( 'path' );
-const chalk = require( 'chalk' );
-const webpack = require( 'webpack' );
-const fileSize = require( 'filesize' );
-const gzipSize = require( 'gzip-size' );
-const resolvePkg = require( 'resolve-pkg' );
-const config = require( '../config/webpack.config.prod' );
-const cgbDevUtilsPath = resolvePkg( 'cgb-dev-utils', { cwd: __dirname } );
-const clearConsole = require( cgbDevUtilsPath + '/clearConsole' );
+const fs = require( 'fs' )
+const ora = require( 'ora' )
+const path = require( 'path' )
+const chalk = require( 'chalk' )
+const webpack = require( 'webpack' )
+const copyDir = require( 'copy-dir' )
+const fileSize = require( 'filesize' )
+const gzipSize = require( 'gzip-size' )
+const resolvePkg = require( 'resolve-pkg' )
+const config = require( '../config/webpack.config.prod' )
+const cgbDevUtilsPath = resolvePkg( 'cgb-dev-utils', { cwd: __dirname } )
+const clearConsole = require( cgbDevUtilsPath + '/clearConsole' )
 const formatWebpackMessages = require( cgbDevUtilsPath +
-	'/formatWebpackMessages' );
+	'/formatWebpackMessages' )
 
 // Build file paths.
-const theCWD = process.cwd();
-const fileBuildJS = path.resolve( theCWD, './dist/blocks.build.js' );
-const fileRendererJS = path.resolve( theCWD, './dist/renderer.build.js' );
-const fileEditorCSS = path.resolve( theCWD, './dist/blocks.editor.build.css' );
-const fileStyleCSS = path.resolve( theCWD, './dist/blocks.style.build.css' );
+const theCWD = process.cwd()
+const fileBuildJS = path.resolve( theCWD, './dist/blocks.build.js' )
+const fileEditorCSS = path.resolve( theCWD, './dist/blocks.editor.build.css' )
+const fileStyleCSS = path.resolve( theCWD, './dist/blocks.style.build.css' )
+
+// Exclude these files from the outputted plugin files
+const srcFilesIgnore = [ 'blocks.js', 'renderer.js', 'common.scss' ]
 
 /**
  * Get File Size
@@ -48,13 +51,13 @@ const fileStyleCSS = path.resolve( theCWD, './dist/blocks.style.build.css' );
  * @returns {string} then size result.
  */
 const getFileSize = filePath => {
-	return fileSize( gzipSize.sync( fs.readFileSync( filePath ) ) );
+	return fileSize( gzipSize.sync( fs.readFileSync( filePath ) ) )
 };
 
-clearConsole();
+clearConsole()
 
 // Init the spinner.
-const spinner = new ora( { text: '' } );
+const spinner = new ora( { text: '' } )
 
 /**
  * Build function
@@ -64,31 +67,33 @@ const spinner = new ora( { text: '' } );
  * @param {json} webpackConfig config
  */
 async function build( webpackConfig ) {
+	clearConsole()
+
 	// Compiler Instance.
-	const compiler = await webpack( webpackConfig );
+	const compiler = await webpack( webpackConfig )
 
 	// Run the compiler.
 	compiler.run( ( err, stats ) => {
-		clearConsole();
+		copySourceFiles( srcFilesIgnore )
 
 		if ( err ) {
-			return console.log( err );
+			return console.log( err )
 		}
 
 		// Get the messages formatted.
-		const messages = formatWebpackMessages( stats.toJson( {}, true ) );
+		const messages = formatWebpackMessages( stats.toJson( {}, true ) )
 
 		// If there are errors just show the errors.
 		if ( messages.errors.length ) {
 			// Only keep the first error. Others are often indicative
 			// of the same problem, but confuse the reader with noise.
 			if ( messages.errors.length > 1 ) {
-				messages.errors.length = 1;
+				messages.errors.length = 1
 			}
 			// Formatted errors.
-			clearConsole();
-			console.log( '\n❌ ', chalk.black.bgRed( ' Failed to compile build. \n' ) );
-			console.log( '\n👉 ', messages.errors.join( '\n\n' ) );
+			clearConsole()
+			console.log( '\n❌ ', chalk.black.bgRed( ' Failed to compile build. \n' ) )
+			console.log( '\n👉 ', messages.errors.join( '\n\n' ) )
 
 			// Don't go beyond this point at this time.
 			return;
@@ -106,29 +111,22 @@ async function build( webpackConfig ) {
 					'\nTreating warnings as errors because process.env.CI = true.\n' +
 						'Most CI servers set it automatically.\n'
 				)
-			);
-			console.log( messages.warnings.join( '\n\n' ) );
+			)
+			console.log( messages.warnings.join( '\n\n' ) )
 		}
 
 		// Start the build.
-		console.log( `\n ${ chalk.dim( 'Let\'s build and compile the files...' ) }` );
-		console.log( '\n✅ ', chalk.black.bgGreen( ' Built successfully! \n' ) );
+		console.log( `\n ${ chalk.dim( 'Let\'s build and compile the files...' ) }` )
+		console.log( '\n✅ ', chalk.black.bgGreen( ' Built successfully! \n' ) )
 
 		console.log(
 			'\n\n',
 			'File sizes after gzip:',
 			'\n\n',
-
 			getFileSize( fileBuildJS ),
 			`${ chalk.dim( '— ./dist/' ) }`,
 			`${ chalk.green( 'blocks.build.js' ) }`,
 			'\n',
-
-			getFileSize( fileRendererJS ),
-			`${ chalk.dim( '— ./dist/' ) }`,
-			`${ chalk.green( 'blocks.renderer.js' ) }`,
-			'\n',
-
 			getFileSize( fileEditorCSS ),
 			`${ chalk.dim( '— ./dist/' ) }`,
 			`${ chalk.green( 'blocks.editor.build.css' ) }`,
@@ -139,10 +137,34 @@ async function build( webpackConfig ) {
 			`${ chalk.green( 'blocks.style.build.css' ) }`,
 
 			'\n\n'
-		);
+		)
 
-		return true;
-	} );
+		return true
+	} )
 }
 
-build( config );
+async function copySourceFiles( excludeFiles ) {
+	const distFolder = path.resolve( theCWD, './dist/' )
+	const srcFolder = path.resolve( theCWD, './src/' )
+
+	copyDir.sync( srcFolder, distFolder, ( stat, filepath, filename ) => {
+		const relativePath = filepath.replace( `${ srcFolder }/`, '' )
+		const keep = ! ( relativePath.indexOf( '/' ) !== -1 || excludeFiles.indexOf( filename ) !== -1 || path.extname( filepath ) === '' )
+
+		if ( keep ) {
+			console.log(
+				'Copied',
+				`${ chalk.dim( `— ${ relativePath }` ) }`,
+				`${ chalk.green( filename ) }` )
+		}
+		return keep
+	}, err => err )
+
+	fs.rename( `${ distFolder }/init.php`, `${ distFolder }/plugin.php`, ( err ) => {
+		if ( err ) {
+			throw err
+		}
+	} )
+}
+
+build( config )
