9f1ac49441c4622a10de5d68af203d24b0d5a6a1
Ken Eucker <keneucker@gmail.com>
2019-05-19 02:05:01 -0700

    Updated all dependencies to @babel/core compatible dependencies.

--- index.js
+++ index.js
@@ -3,65 +3,65 @@
  */
 
 // Update notifier.
-const updateNotifier = require( 'update-notifier' );
-const pkg = require( './package.json' );
-updateNotifier( { pkg } ).notify();
+const updateNotifier = require('update-notifier');
+const pkg = require('./package.json');
+updateNotifier({ pkg }).notify();
 
 module.exports = {
-	presets: [
-		'@babel/react',
-		[
-			'@babel/env',
-			{
-				modules: false,
-				targets: {
-					browsers: [
-						'last 2 Chrome versions',
-						'last 2 Firefox versions',
-						'last 2 Safari versions',
-						'last 2 iOS versions',
-						'last 1 Android version',
-						'last 1 ChromeAndroid version',
-						'ie 11',
-					],
-				},
-			},
-		],
-	],
-	plugins: [
-		'@babel/plugin-syntax-dynamic-import',
-		'@babel/plugin-syntax-import-meta',
-		[
-			'@babel/plugin-proposal-object-rest-spread',
-			{
-				useBuiltIns: true,
-			},
-		],
-		[
-			'@babel/plugin-proposal-class-properties',
-			{
-				loose: true,
-			},
-		],
-		'@babel/plugin-proposal-json-strings',
-		[
-			'@babel/plugin-proposal-decorators',
-			{
-				legacy: true,
-			},
-		],
-		[
-			'babel-plugin-transform-runtime',
-			{
-				helpers: false,
-				polyfill: false,
-				regenerator: true,
-			},
-		],
-		'@babel/plugin-proposal-function-sent',
-		'@babel/plugin-proposal-export-namespace-from',
-		'@babel/plugin-proposal-numeric-separator',
-		'@babel/plugin-proposal-throw-expressions',
-		'@wordpress/babel-plugin-import-jsx-pragma',
-	],
+    presets: [
+        '@babel/react',
+        [
+            '@babel/env',
+            {
+                modules: false,
+                targets: {
+                    browsers: [
+                        'last 2 Chrome versions',
+                        'last 2 Firefox versions',
+                        'last 2 Safari versions',
+                        'last 2 iOS versions',
+                        'last 1 Android version',
+                        'last 1 ChromeAndroid version',
+                        'ie 11',
+                    ],
+                },
+            },
+        ],
+    ],
+    plugins: [
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-syntax-import-meta',
+        [
+            '@babel/plugin-proposal-object-rest-spread',
+            {
+                useBuiltIns: true,
+            },
+        ],
+        [
+            '@babel/plugin-proposal-class-properties',
+            {
+                loose: true,
+            },
+        ],
+        '@babel/plugin-proposal-json-strings',
+        [
+            '@babel/plugin-proposal-decorators',
+            {
+                legacy: true,
+            },
+        ],
+        [
+            '@babel/plugin-transform-runtime',
+            {
+                helpers: false,
+                polyfill: false,
+                regenerator: true,
+            },
+        ],
+        '@babel/plugin-proposal-function-sent',
+        '@babel/plugin-proposal-export-namespace-from',
+        '@babel/plugin-proposal-numeric-separator',
+        '@babel/plugin-proposal-throw-expressions',
+        '@wordpress/babel-plugin-import-jsx-pragma',
+    ],
 };
